44d7514a068ad1cbd2cfaa5debb35e5ce96ef7ef
Ondrej Lexa <lexa.ondrej@gmail.com>
2014-12-09 13:24:22 +0100

    v0.2.0 release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='apsg',
-    version='0.2.0devel',
+    version='0.2.0',
     description='APSG - structural geology module for Python',
     long_description=readme + '\n\n' + history,
     author='Ondrej Lexa',
