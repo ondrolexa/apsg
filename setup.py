bcd1d4dd6050643d468c9d6f52d8de512729966c
Ondrej Lexa <lexa.ondrej@gmail.com>
2017-11-18 14:12:13 +0100

    Bump version: 0.4.3 → 0.4.4

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='apsg',
-    version='0.4.3',
+    version='0.4.4',
     description='APSG - structural geology module for Python',
     long_description=readme + '\n\n' + history,
     author='Ondrej Lexa',
