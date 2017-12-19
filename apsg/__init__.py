de6b57a9102d255de912224ec1b520d4914f5ef8
David Landa <david.landa@protonmail.ch>
2017-12-19 18:06:08 +0100

    Define version inside package and use it in setup.py

--- apsg/__init__.py
+++ apsg/__init__.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 from .core import (Vec3, Fol, Lin, Pair, Fault,
                    Group, PairSet, FaultSet,
                    Ortensor, Cluster, StereoGrid, G)
+
 from .plotting import StereoNet, FabricPlot
 from .tensors import DefGrad, VelGrad, Stress
 from .db import SDB
 from .helpers import sind, cosd, tand, acosd, asind, atand, atan2d
 
+
+__version__ = '0.5.1'
 __author__ = 'Ondrej Lexa'
 __email__ = 'lexa.ondrej@gmail.com'
