5cbf57c1ad887fc6607407af3f5364ab8148c512
Ondrej Lexa <lexa.ondrej@gmail.com>
2018-12-10 20:50:36 +0100

    pytest-runner in setup_requires

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 import sys
 from setuptools import setup, find_packages
-
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -15,17 +13,12 @@ with open('HISTORY.rst') as history_file:
 requirements = [
     'numpy',
     'matplotlib',
-    'scipy',
-    'pytest-runner'
+    'scipy'
 ]
-
 
 # Recipe from https://pypi.org/project/pytest-runner/
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
-
-tests_requirements = [] + pytest_runner
-
 
 setup(
     name='apsg',
@@ -53,5 +46,6 @@ setup(
         'Programming Language :: Python :: 3',
     ],
     test_suite='tests',
-    tests_require=tests_requirements
+    setup_requires=pytest_runner,
+    tests_require=['pytest']
 )
