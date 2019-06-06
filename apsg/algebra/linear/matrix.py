aa05cdcbbbe5021d363c4fefcb6d3b3d6148a6d5
Landa David <david.landa@rozhlas.cz>
2019-06-06 15:40:48 +0200

    Move comment to init file

--- apsg/algebra/linear/matrix.py
+++ apsg/algebra/linear/matrix.py
@@ -3,41 +3,6 @@
 
 """
 Matrix algebra types and functions.
-
-## Fact, Terms and Definitions
-
-> A *matrix* is rectangular array of items arranged in rows and columns.
-
-Our implementation is focused on real matrices, that is, matrix elements are
-real (complex) numbers represented `by float` type.
-
-- Matrix Dimension: The number of rows and columns that a matrix has is
-  called its dimension or its order. By convention, rows are listed first;
-  and columns, second.
-
-- Identity Matrix: ...
-
-- A *square matrix* is a matrix with the same number of rows and columns (n × n).
-
-- A *square diagonal matrix* is a square matrix 
-
-- A *scalar matrix* is a square diagonal matrix with equal main diagonal entries.
-
-- Row Vector: Matrix of dimension 1 × n.
-
-- Column Vector: Matrix of dimension n × 1
-
-- Idempotent Matrix: ...
-
-- Rectangular Diagonal Matrix
-
-- Symmetric Diagonal Matrix
-
-- Size - The *size* of a matrix is defined by the number of rows and columns that it contains.
-- Dimensions - A matrix with m rows and n columns is called an m × n matrix or m-by-n matrix, while m and n are called its dimensions.
-- Row Vector - Matrices with a single row are called row vectors
-- Empty Matrix - In some contexts, such as computer algebra programs, it is useful to consider a matrix with no rows or no columns, called an empty matrix.
-
 """
 
 
