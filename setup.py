435a335e012b00bf1c18284029bed1429b6a11c1
David Landa <david.landa@protonmail.com>
2020-01-26 18:10:21 +0100

    Add src folder

--- setup.py
+++ setup.py
@@ -5,35 +5,52 @@ import sys
 from os import path
 from setuptools import setup, find_packages
 
-this_directory = path.abspath(path.dirname(__file__))
 
-with open(path.join(this_directory, 'README.md')) as f:
-    readme = f.read()
+CURRENT_PATH = path.abspath(path.dirname(__file__))
 
-with open(path.join(this_directory, 'HISTORY.md')) as f:
-    history = f.read()
 
-requirements = [
-    'numpy',
-    'matplotlib',
-    'scipy'
-]
+with open(path.join(CURRENT_PATH, 'README.md')) as file:
+    readme = file.read()
+
+
+with open(path.join(CURRENT_PATH, 'HISTORY.md')) as file:
+    history = file.read()
+
 
 # Recipe from https://pypi.org/project/pytest-runner/
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
 
+
 setup(
     name='apsg',
     version='0.6.3',
-    description='APSG - structural geology module for Python',
+    description='APSG - The package for structural geologists',
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/markdown',
     author='Ondrej Lexa',
     author_email='lexa.ondrej@gmail.com',
     url='https://github.com/ondrolexa/apsg',
-    packages=find_packages(),
-    install_requires=requirements,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "matplotlib",
+        "numpy>=1.14",
+        "scipy>=1.0",
+    ],
+    extras_require={
+        "testing": [
+            "pytest",
+            "radon",
+        ],
+        "linting": [
+            "black",
+            "pylint",
+        ],
+        "jupyter": [
+            "jupyterlab",
+        ]
+    },
     entry_points="""
     [console_scripts]
     iapsg=apsg.shell:main
@@ -47,7 +64,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     setup_requires=pytest_runner,
