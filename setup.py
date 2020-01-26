fe923670e502216e6408107ce898fab400e32e95
David Landa <david.landa@protonmail.com>
2020-01-26 18:18:46 +0100

    Format the setup.py file

--- setup.py
+++ setup.py
@@ -9,47 +9,35 @@ from setuptools import setup, find_packages
 CURRENT_PATH = path.abspath(path.dirname(__file__))
 
 
-with open(path.join(CURRENT_PATH, 'README.md')) as file:
+with open(path.join(CURRENT_PATH, "README.md")) as file:
     readme = file.read()
 
 
-with open(path.join(CURRENT_PATH, 'HISTORY.md')) as file:
+with open(path.join(CURRENT_PATH, "HISTORY.md")) as file:
     history = file.read()
 
 
 # Recipe from https://pypi.org/project/pytest-runner/
-needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
-pytest_runner = ['pytest-runner'] if needs_pytest else []
+needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
 
 
 setup(
-    name='apsg',
-    version='0.6.3',
-    description='APSG - The package for structural geologists',
-    long_description=readme + '\n\n' + history,
-    long_description_content_type='text/markdown',
-    author='Ondrej Lexa',
-    author_email='lexa.ondrej@gmail.com',
-    url='https://github.com/ondrolexa/apsg',
+    name="apsg",
+    version="0.6.3",
+    description="APSG - The package for structural geologists",
+    long_description=readme + "\n\n" + history,
+    long_description_content_type="text/markdown",
+    author="Ondrej Lexa",
+    author_email="lexa.ondrej@gmail.com",
+    url="https://github.com/ondrolexa/apsg",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=[
-        "matplotlib",
-        "numpy>=1.14",
-        "scipy>=1.0",
-    ],
+    install_requires=["matplotlib", "numpy>=1.14", "scipy>=1.0",],
     extras_require={
-        "testing": [
-            "pytest",
-            "radon",
-        ],
-        "linting": [
-            "black",
-            "pylint",
-        ],
-        "jupyter": [
-            "jupyterlab",
-        ]
+        "testing": ["pytest", "radon",],
+        "linting": ["black", "pylint",],
+        "jupyter": ["jupyterlab",],
     },
     entry_points="""
     [console_scripts]
@@ -57,18 +45,18 @@ setup(
     """,
     license="MIT",
     zip_safe=False,
-    keywords='apsg',
+    keywords="apsg",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
     ],
-    test_suite='tests',
+    test_suite="tests",
     setup_requires=pytest_runner,
-    tests_require=['pytest']
+    tests_require=["pytest"],
 )
